0564ae952f5f0185d645a8e890d19735999877ab
Neha Bhole <nehasbhole@gmail.com>
2021-01-13 14:31:38 -0500

    added loop to count total votes and list candidate options

--- PyPoll.py
+++ PyPoll.py
@@ -15,7 +15,30 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# Initialize a total vote counter
+total_votes = 0
+
+# Candidate Options
+candidate_options = []
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
-    # To do: read and analyze the data here.
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+    
+    # Read and print the header row.
+    headers = next(file_reader)
+    
+    # Print each row in the CSV file.
+    for row in file_reader:
+        total_votes += 1
+
+        # Print the candidate name from each row
+        candidate_name = row[2]
+        
+        # Add the candidate name to the candidate list.
+        candidate_options.append(candidate_name)
+
+# Print the candidates
+print(candidate_options)
